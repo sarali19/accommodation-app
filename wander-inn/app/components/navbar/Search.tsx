8117c2b927bb0eaf8e5581c3a835edb616446556
El Yazid Lissaoui <elyazidlissaoui@gmail.com>
2026-05-08 18:59:04 +0100

    Create responsive navbar component

--- wander-inn/app/components/navbar/Search.tsx
+++ wander-inn/app/components/navbar/Search.tsx
@@ -10,7 +10,7 @@ export default function Search() {
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">Any week</div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
           <div className="hidden sm:block">Add guests</div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
+          <div className="p-2 bg-green-900 rounded-full text-white">
             <BiSearch size={18} />
           </div>
         </div>
--- wander-inn/app/components/navbar/UserMenu.tsx
+++ wander-inn/app/components/navbar/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 export default function UserMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,7 +33,10 @@ export default function UserMenu() {
       </div>
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">yo</div>
+          <div className="flex flex-col cursor-pointer">
+            <MenuItem onClick={() => {}} label="Login" />
+            <MenuItem onClick={() => {}} label="Sign up" />
+          </div>
         </div>
       )}
     </div>
