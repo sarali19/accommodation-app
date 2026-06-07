7c4b967bff014152d3a2ab9e25e3504f4437a0ca
El Yazid Lissaoui <elyazidlissaoui@gmail.com>
2026-06-07 13:16:30 +0100

    Login modal configuration

---
+++ wander-inn/app/api/register/route.ts
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const { email, name, password } = body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("REGISTER ERROR:", error);
+
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
---
+++ wander-inn/app/components/modals/LoginModal.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+
+export default function LoginModal() {
+  const router = useRouter();
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: FieldValues) => {
+    try {
+      setIsLoading(true);
+
+      const callback = await signIn("credentials", {
+        ...data,
+        redirect: false,
+      });
+
+      if (callback?.ok) {
+        toast.success("Logged in");
+        router.refresh();
+        loginModal.onClose();
+      }
+
+      if (callback?.error) {
+        toast.error(callback.error);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome back" subtitle="Login to your account!" center />
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
+      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already have an account?</div>
+          <div className="text-neutral-800 cursor-pointer hover:underline" onClick={registerModal.onClose}>
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)} //React Hook Form, validate the form and prepare the data before running my submit logic
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+}
