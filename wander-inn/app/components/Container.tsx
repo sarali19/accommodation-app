e82097ed448ec3d0982d7959c9921707c093b1e7
Sara Lissaoui <saralissaoui@gmail.com>
2026-07-02 23:57:43 +0100

    Feat: Listing card, favoriting, individual listing view

--- wander-inn/app/components/Container.tsx
+++ wander-inn/app/components/Container.tsx
@@ -4,7 +4,6 @@ interface ContainerProps {
   children: React.ReactNode;
 }
 
-//tsrfc
 export default function Container({ children }: ContainerProps) {
   return <div className="max-w-[2580px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">{children}</div>;
 }
---
+++ wander-inn/app/components/EmptyState.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Heading from "./Heading";
+import Button from "./Button";
+
+interface EmptyStateProps {
+  title?: string;
+  subTitle?: string;
+  showReset?: boolean;
+}
+
+export default function EmptyState({
+  title = "No exact matches",
+  subTitle = "try changing or removing some of your filters",
+  showReset,
+}: EmptyStateProps) {
+  const router = useRouter();
+
+  return (
+    <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
+      <Heading center title={title} subtitle={subTitle} />
+      <div className="w-48 mt-4">
+        {showReset && <Button outline label="Remove all filters" onClick={() => router.push("/")} />}
+      </div>
+    </div>
+  );
+}
