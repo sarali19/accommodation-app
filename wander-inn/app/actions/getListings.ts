9e2c1be167a236389ea8460b0738a24ff353a7bc
Sara Lissaoui <saralissaoui@gmail.com>
2026-07-06 01:13:18 +0100

    Loading user's listings with server components

--- wander-inn/app/actions/getListings.ts
+++ wander-inn/app/actions/getListings.ts
@@ -1,8 +1,37 @@
 import prisma from "@/app/libs/prismadb";
 
-export default async function getListings() {
+export interface IListingParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
+}
+
+export default async function getListings(params: IListingParams) {
   try {
+    const {
+      userId,
+      //  roomCount,
+      //  guestCount,
+      //  bathroomCount,
+      //  startDate,
+      //  endDate,
+      //  locationValue,
+      //  category,
+    } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: { createdAt: "desc" },
     });
 
